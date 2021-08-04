456a814f22244702801197e9e7949c20ef8effa1
YangSungSim <yangsungsim227@gmail.com>
2021-08-04 14:10:42 +0900

    second commit
    second commit

---
+++ front-end/src/views/RegisterPage2.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class='container'>
+    <div class='row justify-content-center'>
+      <img class="logo" src="/static/images/logo.png">
+    </div>
+  </div>
+</template>
+
+export default {
+  name: 'RegisterPage',
+
+}
